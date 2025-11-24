2f5e73725b23dc995db988d4bf6fecf2838df440
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2025-11-23 23:23:16 -0300

    feat: corrigir importação do componente Container e atualizar conteúdo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
+import { Container } from "./components/container";
+
 
 export function App() {
   return (
     <>
-      <Container>lorem</Container>
+      <Container>ipsum</Container>
     </>
   );
 }
