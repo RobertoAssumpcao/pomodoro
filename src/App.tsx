e8cd64f06dac2ea9086715dc3b3f7ecc84f8a841
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2025-11-26 23:24:32 -0300

    feat: adicionar componentes de formulário e estilos para entrada de dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import "./App.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import {CountDown} from "./components/CountDown";
+import {PlayIcon} from "lucide-react";
+import {DefaultInput} from "./components/DefaultInput";
+import {Cycles} from "./components/Cycles";
 
 export function App() {
   return (
@@ -18,6 +22,28 @@ export function App() {
 
         <Container>
             <CountDown/>
+        </Container>
+
+        <Container>
+            <form className='form' action="">
+                <div className='formRow'>
+                    <DefaultInput labelText='Task teste' id='input' type='text' placeholder='Digite algo'/>
+                </div>
+
+                <div className='formRow'>
+                    <p>Lorem</p>
+                </div>
+
+                <div className='formRow'>
+                    <Cycles></Cycles>
+                </div>
+
+                <div className='formRow'>
+                    <button>
+                        <PlayIcon/>
+                    </button>
+                </div>
+            </form>
         </Container>
     </>
   );
