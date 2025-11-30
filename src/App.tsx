bb297699eb8c9b5e6296edfed4e0fa9b98bbde74
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2025-11-29 23:41:45 -0300

    component defaultbutton

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import "./App.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
-import {CountDown} from "./components/CountDown";
-import {PlayIcon} from "lucide-react";
-import {DefaultInput} from "./components/DefaultInput";
-import {Cycles} from "./components/Cycles";
+import { CountDown } from "./components/CountDown";
+import { PlayCircleIcon } from "lucide-react";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
 
 export function App() {
   return (
@@ -16,35 +17,38 @@ export function App() {
         <Logo />
       </Container>
 
-        <Container>
-            <Menu></Menu>
-        </Container>
+      <Container>
+        <Menu></Menu>
+      </Container>
 
-        <Container>
-            <CountDown/>
-        </Container>
+      <Container>
+        <CountDown />
+      </Container>
 
-        <Container>
-            <form className='form' action="">
-                <div className='formRow'>
-                    <DefaultInput labelText='Task teste' id='input' type='text' placeholder='Digite algo'/>
-                </div>
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="Task teste"
+              id="input"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
 
-                <div className='formRow'>
-                    <p>Lorem</p>
-                </div>
+          <div className="formRow">
+            <p>Lorem</p>
+          </div>
 
-                <div className='formRow'>
-                    <Cycles></Cycles>
-                </div>
+          <div className="formRow">
+            <Cycles></Cycles>
+          </div>
 
-                <div className='formRow'>
-                    <button>
-                        <PlayIcon/>
-                    </button>
-                </div>
-            </form>
-        </Container>
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon />}></DefaultButton>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -6,11 +6,11 @@ type DefaultInputProps = {
     labelText: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({id, labelText, type, ...rest }: DefaultInputProps) {
+export function DefaultInput({id, labelText, type, ...props }: DefaultInputProps) {
     return (
         <>
             <label htmlFor={id}>{labelText}</label>
-            <input className={styles.input} id={id} type={type} {...rest} />
+            <input className={styles.input} id={id} type={type} {...props} />
         </>
     )
 }
