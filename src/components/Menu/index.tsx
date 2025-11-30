713629ed7b41c130651146494dad67c1a45715b5
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2025-11-30 20:21:20 -0300

    feat: support dark and light theme.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,55 @@
-import {HouseIcon, HistoryIcon, SettingsIcon, SunIcon} from "lucide-react";
-import styles from './styles.module.css';
+import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from "lucide-react";
+import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-    return (
-        <nav className={styles.menu} >
-            <a className={styles.menuLink} href='#'>
-                <HouseIcon/>
-            </a>
-            <a className={styles.menuLink} href='#'>
-                <HistoryIcon/>
-            </a>
-            <a className={styles.menuLink} href='#'>
-                <SettingsIcon/>
-            </a>
-            <a className={styles.menuLink} href='#'>
-                <SunIcon/>
-            </a>
-        </nav>
-    )
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function toggleTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Botão para Home"
+        title="Botão para Home"
+      >
+        <HouseIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Botão para Histórico"
+        title="Botão para Histórico"
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Botão para Configurações"
+        title="Botão para Configurações"
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Botão para Alternar Tema"
+        title="Botão para Alternar Tema"
+        onClick={toggleTheme}
+      >
+        <SunIcon />
+      </a>
+    </nav>
+  );
 }
